7238904331fb04382adad7bf569aef82a0bc56b2
Harish <harishpoctor@gmail.com>
2023-06-05 11:51:02 +0530

    Improved InBuiltApps list page ui.

--- src/pages/inBuiltApps.jsx
+++ src/pages/inBuiltApps.jsx
@@ -2,26 +2,27 @@ import { Link } from "react-router-dom";
 import "./inBuiltApps.css";
 
 const InBuiltApps = () => {
+  const apps = [
+    { link: "/apps/calculator", name: "Calculator" },
+    { link: "/apps/rock-paper-scissor", name: "Rock Paper Scissor Game" },
+    { link: "/apps/stopwatch", name: "Stopwatch" },
+    { link: "/apps/password-generator", name: "Password Generator" },
+    { link: "/apps/tic-tac-toe", name: "Tic-Tac-Toe Game" },
+  ];
   return (
     <div className="iba_main">
       <h1>In Built Apps</h1>
-      <ul>
-        <li>
-          <Link to="/apps/calculator">Calculator</Link>
-        </li>
-        <li>
-          <Link to="/apps/rock-paper-scissor">Rock Paper Scissor Game</Link>
-        </li>
-        <li>
-          <Link to="/apps/stopwatch">Stopwatch</Link>
-        </li>
-        <li>
-          <Link to="/apps/password-generator">Password Generator</Link>
-        </li>
-        <li>
-          <Link to="/apps/tic-tac-toe">Tic-Tac-Toe Game</Link>
-        </li>
-      </ul>
+      <div class="list-group">
+        {apps.map((item, idx) => (
+          <Link
+            key={idx}
+            to={item.link}
+            className="list-group-item list-group-item-action"
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
